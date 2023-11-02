3cf71a1d0075d956fe44b35eacb39f23e3781720
unknown <gausulislam5@gmail.com>
2023-11-02 23:13:52 +0600

    update data loader in mongo db and  express js

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../Firebase/firebase.config';
 
 
@@ -21,6 +21,10 @@ const AuthProvider = ({children}) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth,email,password)
     }
+
+    const logOut=()=>{
+        return signOut(auth);
+    }
     useEffect(()=>{
        const unsubscribe= onAuthStateChanged(auth, currentUser=>{
             setUsers(currentUser);
@@ -36,6 +40,7 @@ const AuthProvider = ({children}) => {
         loading,
         createUser,
         login,
+        logOut,
     }
 
     return (
